660f8abd16b1daf9743614f8aea9fd12094dd60f
Karan <karan.n.j.kumar@gmail.com>
2021-11-08 18:29:17 +0000

    Add day1

---
+++ day1/day1.py
@@ -0,0 +1,14 @@
+def calc_product(lines: [str]) -> int:
+    for index, line in enumerate(lines):
+        entry = int(line.strip())
+        if entry > 2020:
+            continue
+        for line2 in lines[index+1:]:
+            if entry + int(line2) == 2020:
+                return entry * int(line2)
+
+
+if __name__ == '__main__':
+    file = open('day1input.txt', 'r')
+    product = calc_product(file.readlines())
+    print(product)
