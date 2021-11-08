8fbcf6ba6bdd93c9648278df3fa751b365ad8bfe
Karan <karan.n.j.kumar@gmail.com>
2021-11-08 19:27:13 +0000

    Add day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,4 @@
-def count_valid_passwords(lines: [str]) -> int:
+def count_valid_passwords_part1(lines: [str]) -> int:
     valid = 0
     for line in lines:
         split = line.split(' ')
@@ -15,7 +15,28 @@ def count_valid_passwords(lines: [str]) -> int:
     return valid
 
 
+def count_valid_passwords_part2(lines: [str]) -> int:
+    valid = 0
+    for line in lines:
+        split = line.split(' ')
+        occurrences = split[0].split('-')
+
+        # -1 because they are 0-indexed
+        position1 = int(occurrences[0]) - 1
+        position2 = int(occurrences[1]) - 1
+        letter = (split[1])[0]
+        password = split[-1]
+
+        count = 1 if password[position1] == letter else 0
+        count = count + (1 if password[position2] == letter else 0)
+        if count == 1:
+            valid = valid + 1
+
+    return valid
+
+
 if __name__ == '__main__':
     with open('day2input.txt', 'r') as file:
-        num_valid = count_valid_passwords(file.readlines())
-        print(num_valid)
+        file_lines = file.readlines()
+        print('Part 1', count_valid_passwords_part1(file_lines))
+        print('Part 2', count_valid_passwords_part2(file_lines))
